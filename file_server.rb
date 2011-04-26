8da249a2415c5178043645084f7434add1899cdf
ph0rque <andrei.shindyapin@gmail.com>
2011-04-25 22:52:44 -0400

    works now...

--- file_server.rb
+++ file_server.rb
@@ -1,3 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 
+enable :static
+
+get '/' do
+  "Links to assigments coming shortly!"
+end
+
+#get '/assignment/:assign_number' do
+#  File.read(File.join('assigment', "#{params[:assign_number]}.html"))
+#end
