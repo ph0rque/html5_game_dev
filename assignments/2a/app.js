2a1aa1e6660817f86947814132fa255c51d61e25
ph0rque <andrei.shindyapin@gmail.com>
2011-04-30 22:37:54 -0400

    bask in the awesomeness that is assig 2a!

--- assignments/2a/app.js
+++ assignments/2a/app.js
@@ -1,28 +1,33 @@
 $(this).keydown(function(e) {
   key = e.keyCode;
   img = $('#the-img');
+  notice = $('#notice');
   
   if (key == 37) {
+    notice.text("←");
     img.animate(
       {left: '-=10' },
       100
     );
   } else if (key == 38) {
+    notice.text("↑");
     img.animate(
       {top: '-=10' },
       100
     );
   } else if (key == 39) {
+    notice.text("→");
     img.animate(
       {left: '+=10' },
       100
     );
   } else if (key == 40) {
+    notice.text("↓");
     img.animate(
       {top: '+=10' },
       100
     );
   } else {
-    //do nothing
+    notice.text("Sorry bub, that's not one of the arrow keys :~/");
   }
 });
