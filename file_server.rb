3fb0b7ae7e42c43d5d32b19718e9a79ba97118d7
ph0rque <andrei.shindyapin@gmail.com>
2011-05-11 00:16:18 -0400

    good start to task 2a

--- file_server.rb
+++ file_server.rb
@@ -6,6 +6,9 @@ get '/' do
 end
 
 get '/tasks/:number/:file' do
+  #  :content_type get_content_from_file_using_gem(params[:file])
+  #  https://github.com/blackwinter/ruby-filemagic
+  
   File.read(File.join('tasks', "#{params[:number]}", "#{params[:file]}"))
 end
 
--- tasks/2a/app.js
+++ tasks/2a/app.js
@@ -1,58 +1,25 @@
-var img = $('#the-img');
-var notice = $('#notice');
+$(document).ready(function() {
 
-//var x_y = img.position();
-var y = 0 // y u no work? x_y.top;
-var x = 0 //same here... x_y.left;
+  var context = $('#game-canvas')[0].getContext('2d');
+  context.fillStyle = "#eeeeff";
+  context.fillRect(0, 0, 1000, 600);
 
-var max_x = $('#the-div').width() - img.width();
-var max_y = $('#the-div').height() - img.height();
-
-$(this).keydown(function(event) {
-  var key = event.keyCode;
-  
-  if (key == 37) {
-    change_notice(notice, "←", 'ok')
-    
-    x - 10 <= 0 ? x = 0 : x -= 10;
-    move(event, img, x, y);
-        
-  } else if (key == 38) {
-    change_notice(notice, "↑", 'ok')
-    
-    y - 10 <= 0 ? y = 0 : y -= 10;
-    move(event, img, x, y);
-  } else if (key == 39) {
-    change_notice(notice, "→", 'ok')
-    
-    x + 10 >= max_x ? x = max_x : x += 10;
-    move(event, img, x, y);
-  } else if (key == 40) {
-    change_notice(notice, "↓", 'ok')
-    
-    y + 10 >= max_y ? y = max_y : y += 10;
-    move(event, img, x, y);
-
-  } else {
-    change_notice(notice, "that's not one of the arrow keys :~/", 'wrong')
-  }
+  context.beginPath();
+  context.moveTo(0,600);
+  context.bezierCurveTo(rand_x(), rand_y(), rand_x(), rand_y(), rand_x(), rand_y());
+  context.bezierCurveTo(rand_x(), rand_y(), rand_x(), rand_y(), rand_x(), rand_y());
+  context.bezierCurveTo(rand_x(), rand_y(), rand_x(), rand_y(), rand_x(), rand_y());
+  context.lineTo(1000,600);
+  context.lineTo(0,600);
+  context.fillStyle = randColor();
+  context.fill();
 });
+//  thing001.bezierCurveTo
 
-function change_notice(notice, content, status) {
-  var color = '#eeeeff';
-  
-    if (status == 'wrong') { color = '#FF6448'; }
-    notice.css("background-color", color);
-    
-    notice.text(content);
-}
+function rand_x() {return Math.floor (Math.random() * 1000 + 1);}
+function rand_y() {return Math.floor (Math.random() * 600 + 1);}
+function rand(maximum) {return Math.floor (Math.random() * maximum + 1);}
 
-function move(event, img, ex, yy) {
-  event.preventDefault();
-
-  img.css(
-    {
-      top: yy,
-      left: ex
-    });
+function randColor() {
+  return "rgba(" + rand(255) + "," + rand(255) + "," + rand(255) + "," + rand(10)/10 + ")";
 }
